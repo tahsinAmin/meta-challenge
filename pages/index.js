0c876e46f636560f068b50377d7aa20316beff05
tahsinAmin <muhammadtahsinamin@gmail.com>
2022-02-08 22:49:37 +0600

    Building the Login Functionality

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Login from "../components/Login";
+import { useMoralis } from "react-moralis";
 
 export default function Home() {
-  const isAuthenticated = false;
+  const { isAuthenticated } = useMoralis();
 
   if (!isAuthenticated) return <Login />;
 
@@ -15,3 +16,5 @@ export default function Home() {
     </div>
   );
 }
+
+// https://youtu.be/3NpZ05sDTQ4?t=3020
