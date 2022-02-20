2990818c8f7e917989d48b7afa3b4512769514cd
tahsinAmin <muhammadtahsinamin@gmail.com>
2022-02-20 19:27:05 +0600

    Building the Messages Component(2)

--- components/Messages.js
+++ components/Messages.js
@@ -1,11 +1,28 @@
-import React, { useReducer, useRef } from "react";
+import React, { useRef } from "react";
 import { ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
+import Message from "./Message";
 import SendMessage from "./SendMessage";
+
+// ONly show messages from the last 15 minutes
+const MINS_DURATION = 15;
 
 export default function Messages() {
   const { user } = useMoralis();
   const endOfMessagesRef = useRef(null);
-  const { data, loading, error } = useMoralisQuery("Messages", (query) => {});
+  const { data, loading, error } = useMoralisQuery(
+    "Messages",
+    (query) =>
+      query
+        .ascending("createdAt")
+        .greaterThan(
+          "createdAt",
+          new Date(Date.now() - 1000 * 60 * MINS_DURATION)
+        ),
+    [],
+    {
+      live: true,
+    }
+  );
   return (
     <div className='pb-56'>
       <div className='my-5'>
@@ -14,7 +31,12 @@ export default function Messages() {
           style={{ marginLeft: "auto", marginRight: "auto" }}
         />
       </div>
-      <div>{/* Each Message */}</div>
+      <div className='space-y-10 p-4'>
+        {/* component now each time you do a map you should have a key this is how react understands that i don't need to re-render every item on that list so the messages i'll need to change the one which has a different key that came in */}
+        {data.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
       <div className='flex justify-center'>
         <SendMessage endOfMessagesRef={endOfMessagesRef} />
       </div>
