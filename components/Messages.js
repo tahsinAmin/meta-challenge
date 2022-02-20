7787d3d0c306ad91cc47b4d9f47d5d5e5b36543b
tahsinAmin <muhammadtahsinamin@gmail.com>
2022-02-20 14:05:56 +0600

    Implementing Auto Scroll Functionality

--- components/Messages.js
+++ components/Messages.js
@@ -1,9 +1,11 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useRef } from "react";
 import { ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
 import SendMessage from "./SendMessage";
 
 export default function Messages() {
   const { user } = useMoralis();
+  const endOfMessagesRef = useRef(null);
+  const { data, loading, error } = useMoralisQuery("Messages", (query) => {});
   return (
     <div className='pb-56'>
       <div className='my-5'>
@@ -14,9 +16,9 @@ export default function Messages() {
       </div>
       <div>{/* Each Message */}</div>
       <div className='flex justify-center'>
-        <SendMessage />
+        <SendMessage endOfMessagesRef={endOfMessagesRef} />
       </div>
-      <div className='text-center text-gray-400 mt-5'>
+      <div ref={endOfMessagesRef} className='text-center text-gray-400 mt-5'>
         <p>You are up to date {user.getUsername()}!</p>
       </div>
     </div>
